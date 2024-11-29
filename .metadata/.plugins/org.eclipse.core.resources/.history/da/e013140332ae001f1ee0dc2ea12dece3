8b1904499c860f9b7dbf548bf686fd867801cfbc
kimsungeun <sungeunkim222@gmail.com>
2024-11-29 18:40:14 +0900

    (p.76)

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/da/e013140332ae001f1ee0dc2ea12dece3
@@ -0,0 +1,11 @@
+package com.studyspringboot.bean;
+
+public class PrinterA implements Printer {
+
+	@Override
+	public void print(String message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Ex02_JavaCodeDI/src/main/java/com/studyspringboot/bean/PrinterA.java
+++ Ex02_JavaCodeDI/src/main/java/com/studyspringboot/bean/PrinterA.java
@@ -4,8 +4,7 @@ public class PrinterA implements Printer {
 
 	@Override
 	public void print(String message) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Printer A : " + message);
 	}
 
 }
